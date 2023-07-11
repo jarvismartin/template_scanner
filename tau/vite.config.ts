ad190277c6e93e681bcfb842bb4bee9e6acb7095
Jarvis Martin <jmartin@solutionsbytext.com>
2023-07-11 18:35:22 -0400

    Set up for GitHub Pages

--- tau/vite.config.ts
+++ tau/vite.config.ts
@@ -3,8 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/tau/",
   plugins: [svelte()],
-  build: {
-    outDir: '../'
-  }
 })
