085aea8141bd5357d395771de4b26cf76052307e
jarvis <jarvism@proton.me>
2023-12-12 17:35:48 -0500

    Add UI bits to let users know there were no issues found

--- tau/src/App.svelte
+++ tau/src/App.svelte
@@ -11,6 +11,7 @@
   let headers = "";
   let limit = 100;
   let min = 0;
+  let nif = false; // No issues found
 
   // These are the GSM chars that
   // count as 2 characters
@@ -52,6 +53,7 @@
 
   function fileComplete(results, file) {
     scanning = true;
+    nif = false;
     console.log("FILE COMPLETE RESULTS:", results);
 
     const { data, errors, meta } = results;
@@ -166,6 +168,9 @@
     csv_url = URL.createObjectURL(blob);
 
     console.log("AT THE END non_gsm:", non_gsm);
+
+    // Tell the user if there are no issues found (nif)
+    if (non_gsm.length === 0) nif = true;
   }
 
   // // Read file as string
@@ -220,7 +225,7 @@
 </script>
 
 <main class="overflow-none">
-  <div class="flex flex-col justify-center p-4 m-auto tau">
+  <div class="relative flex flex-col justify-center p-4 m-auto tau">
     {#if !scanning && non_gsm.length === 0}
       <!-- Drag and Drop -->
       <label
@@ -264,6 +269,28 @@
           on:change={parseFile}
         />
       </label>
+      {#if nif}
+        <div
+          class="w-full flex items-center justify-center flex-col absolute top-36 left-0"
+          in:fade
+        >
+          <div
+            class="flex flex-col items-center justify-center bg-lime-500 w-32 h-32 rounded-full"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              class="w-6 h-6"
+            >
+              <path
+                d="M7.493 18.75c-.425 0-.82-.236-.975-.632A7.48 7.48 0 016 15.375c0-1.75.599-3.358 1.602-4.634.151-.192.373-.309.6-.397.473-.183.89-.514 1.212-.924a9.042 9.042 0 012.861-2.4c.723-.384 1.35-.956 1.653-1.715a4.498 4.498 0 00.322-1.672V3a.75.75 0 01.75-.75 2.25 2.25 0 012.25 2.25c0 1.152-.26 2.243-.723 3.218-.266.558.107 1.282.725 1.282h3.126c1.026 0 1.945.694 2.054 1.715.045.422.068.85.068 1.285a11.95 11.95 0 01-2.649 7.521c-.388.482-.987.729-1.605.729H14.23c-.483 0-.964-.078-1.423-.23l-3.114-1.04a4.501 4.501 0 00-1.423-.23h-.777zM2.331 10.977a11.969 11.969 0 00-.831 4.398 12 12 0 00.52 3.507c.26.85 1.084 1.368 1.973 1.368H4.9c.445 0 .72-.498.523-.898a8.963 8.963 0 01-.924-3.977c0-1.708.476-3.305 1.302-4.666.245-.403-.028-.959-.5-.959H4.25c-.832 0-1.612.453-1.918 1.227z"
+              />
+            </svg>
+            <div class="font-semibold mt-2">No issues found!</div>
+          </div>
+        </div>
+      {/if}
     {:else}
       <div class="flex flex-col justify-between h-full" in:fade>
         {#if scanning}
@@ -399,7 +426,7 @@
               {#each item.Message ? item.Message : item.TemplateFrom.concat(" ").concat(item.TemplateMessage) as char}
                 <pre
                   class="my-2 border-2 border-gray-600 {item.non_gsm.includes(
-                    char
+                    char,
                   )
                     ? 'bg-amber-500 text-slate-700 font-semibold'
                     : ''}">{char}</pre>
